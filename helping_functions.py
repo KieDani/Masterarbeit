a3f60ccacb40ae0047eb89557ce87a854971f0c2
Daniel Kienzle <kienzledaniel@googlemail.com>
2020-09-24 12:10:09 +0200

    added method add_operator

--- helping_functions.py
+++ helping_functions.py
@@ -1,6 +1,7 @@
 import netket as nk
 import scipy
 import numpy as np
+import my_operators as operators
 
 
 #If possible, returns exact energy. Else, it returns None!
@@ -12,3 +13,21 @@ def Lanczos(hamilton, L):
     else:
         print('Too big for exact diagonalization')
         return None
+
+
+#allowed inputs for operator: 'FerroCorr', 'StringCorr', None
+def add_operator(gs, hilbert, L, operator = None):
+    if(operator == 'FerroCorr'):
+        for i in range(1, L):
+            observ_fast = operators.FerroCorrelationZ(hilbert=hilbert, j=0, k=i)
+            name_fast = 'Ferro_correlation_function_fast' + str(i)
+            gs.add_observable(observ_fast, name_fast)
+            return gs
+    elif(operator == 'StringCorr'):
+        for i in range(1, L):
+            observ_fast = operators.FerroCorrelationZ(hilbert=hilbert, j=0, k=i)
+            name_fast = 'Ferro_correlation_function_fast' + str(i)
+            gs.add_observable(observ_fast, name_fast)
+            return gs
+    else:
+        return gs
