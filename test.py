c38db809e44a9b47d897274ce579206f1785d61f
Daniel Kienzle <kienzledaniel@googlemail.com>
2020-10-08 15:33:18 +0200

    put code in test.py into a method -> easier to use with colab

--- test.py
+++ test.py
@@ -11,30 +11,34 @@ __L__ = 50
 __number_samples__ = 1000
 __number_iterations__ = 1000
 
-ha, hi, g = models.build_Heisenbergchain_S1_transformed(L=__L__)
-ha_orig, hi_orig, g_orig = models.build_Heisenbergchain_S1(L=__L__)
-ma, op, sa = machines.JaxFFNN(hilbert=hi, alpha=4, optimizer='Sgd', lr=0.1)
+def run(L=__L__):
+    ha, hi, g = models.build_Heisenbergchain_S1_transformed(L=L)
+    ha_orig, hi_orig, g_orig = models.build_Heisenbergchain_S1(L=L)
+    ma, op, sa = machines.JaxFFNN(hilbert=hi, alpha=4, optimizer='Sgd', lr=0.1)
 
-#TODO: check, why Lanczos does not work for transformed Hamiltonian
-exact_energy = functions.Lanczos(hamilton=ha_orig, L=__L__)
+    #TODO: check, why Lanczos does not work for transformed Hamiltonian
+    exact_energy = functions.Lanczos(hamilton=ha_orig, L=L)
 
-#TODO automaticaly add or remove SR
-sr = nk.optimizer.SR(ma, diag_shift=0.5)
+    #TODO automaticaly add or remove SR
+    sr = nk.optimizer.SR(ma, diag_shift=0.5)
 
-gs = nk.Vmc(hamiltonian=ha, sampler=sa, optimizer=op, n_samples=__number_samples__)#, sr=sr)
-observables = functions.get_operator(hilbert=hi, L=__L__, operator='FerroCorr')
-#observables_slow = functions.get_operator(hilbert=hi, L=__L__, operator='FerroCorr_slow')
-#observables = {**observables, **observables_slow}
-dataname = ''.join(('L', str(__L__)))
-dataname = functions.create_path(dataname)
-print('')
-start = time.time()
-#TODO remove observables from run-method -> Greater speed! But adjust plot to ..._estim.log file!
-gs.run(n_iter=__number_iterations__, out=dataname)#, obs=observables)
-end = time.time()
-print(end - start)
+    gs = nk.Vmc(hamiltonian=ha, sampler=sa, optimizer=op, n_samples=__number_samples__)#, sr=sr)
+    observables = functions.get_operator(hilbert=hi, L=L, operator='FerroCorr')
+    #observables_slow = functions.get_operator(hilbert=hi, L=L, operator='FerroCorr_slow')
+    #observables = {**observables, **observables_slow}
+    dataname = ''.join(('L', str(L)))
+    dataname = functions.create_path(dataname)
+    print('')
+    start = time.time()
+    #TODO remove observables from run-method -> Greater speed! But adjust plot to ..._estim.log file!
+    gs.run(n_iter=__number_iterations__, out=dataname)#, obs=observables)
+    end = time.time()
+    print(end - start)
 
-print('Estimated results:')
-gs2 = nk.Vmc(hamiltonian=ha, sampler=sa, optimizer=op, n_samples=20000)
-gs2.run(n_iter=20, out=''.join((dataname, '_estimate')), obs=observables, write_every=4, save_params_every=4)
-print(gs2.estimate(observables))
+    print('Estimated results:')
+    gs2 = nk.Vmc(hamiltonian=ha, sampler=sa, optimizer=op, n_samples=20000)
+    gs2.run(n_iter=20, out=''.join((dataname, '_estimate')), obs=observables, write_every=4, save_params_every=4)
+    print(gs2.estimate(observables))
+
+
+#run()
