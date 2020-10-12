db31c127b52ae1fce686e9449858e2a109d9ca1b
Daniel Kienzle <kienzledaniel@googlemail.com>
2020-10-12 14:39:12 +0200

    adjusted plot method to use it with *_estimate.log data

--- plot.py
+++ plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 
-def plot(dataname, L):
+def plot(dataname, L, observables=True):
     data=json.load(open(dataname))
     # Extract the relevant information
 
@@ -21,9 +21,9 @@ def plot(dataname, L):
 
     def calcMean(array):
         sum = 0.
-        for i in range(35):
+        for i in range(10):
             sum += array[-i+0]
-        return sum/35.
+        return sum/10.
 
     def getsf(i):
         sf = list()
@@ -31,18 +31,19 @@ def plot(dataname, L):
             sf.append(iteration['Ferro_correlation_function' + str(i)]["Mean"])
         return calcMean(sf)
 
-    for i in range(1, length):
-        sfs_fast.append(getsf(i))
-        xAxis_fast.append(i)
-
     plt.plot(iters, energy)
     plt.show()
 
+    if(observables == True):
+        for i in range(1, length):
+            sfs_fast.append(getsf(i))
+            xAxis_fast.append(i)
 
-    plt.plot(xAxis_fast, sfs_fast)
-    plt.show()
+        plt.plot(xAxis_fast, sfs_fast)
+        plt.show()
+
 
 
 
 #plot(dataname='run/L100.log', L=100)
-plot(dataname='run/L10.log', L=10)
+plot(dataname='run/L20_estimate.log', L=20, observables=True)
