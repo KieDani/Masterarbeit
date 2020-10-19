a895cdbc547b6c5c3a6cf4cae7a12c3fbc71b70c
Daniel Kienzle <kienzledaniel@googlemail.com>
2020-10-19 18:46:21 +0200

    now the expected energy is plotted too

--- plot.py
+++ plot.py
@@ -2,6 +2,7 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+import helping_functions as functions
 
 
 
@@ -48,18 +49,23 @@ def plot(dataname, L, observables=True):
 # Ls should be an array with 4 Elements
 def present(Ls, path):
 
-    def plot4me(a, b, text='Energy-Iterations'):
+    #a = x data, b = f(x), c = expected energy
+    def plot4me(a, b, c, text='Energy-Iterations'):
         plt.subplot(221)  # sublot(Anzahl Zeilen Anzahl Spalten Bild Nummer)
         plt.plot(a[0], b[0])
+        if(c != None): plt.plot(a[0], c[0])
         plt.title(''.join((text, ' ', str(Ls[0]))))
         plt.subplot(222)  # sublot(Anzahl Zeilen Anzahl Spalten Bild Nummer)
         plt.plot(a[1], b[1])
+        if (c != None): plt.plot(a[1], c[1])
         plt.title(''.join((text, ' ', str(Ls[1]))))
         plt.subplot(223)  # sublot(Anzahl Zeilen Anzahl Spalten Bild Nummer)
         plt.plot(a[2], b[2])
+        if (c != None): plt.plot(a[2], c[2])
         plt.title(''.join((text, ' ', str(Ls[2]))))
         plt.subplot(224)  # sublot(Anzahl Zeilen Anzahl Spalten Bild Nummer)
         plt.plot(a[3], b[3])
+        if (c != None): plt.plot(a[3], c[3])
         plt.title(''.join((text, ' ', str(Ls[3]))))
         plt.show()
 
@@ -67,8 +73,9 @@ def present(Ls, path):
     energy_list = []
     sfs_fast_list = []
     xAxis_fast_list = []
-    for i in range(0, len(Ls)):
-        l = Ls[i]
+    expected_energy_list = []
+    for j in range(0, len(Ls)):
+        l = Ls[j]
         print(''.join((path, '/L', str(l), '.log')))
         data_energy = json.load(open(''.join((path, '/L', str(l), '.log'))))
         data_observ = json.load(open(''.join((path, '/L', str(l), '_estimate.log'))))
@@ -101,18 +108,25 @@ def present(Ls, path):
             sfs_fast.append(getsf(i))
             xAxis_fast.append(i)
 
+        tmp = [None, None, None, None, -4.646, -5.830, -7.370, -8.635, -10.125, -11.433, -12.895, -14.230, -15.674]
+        if(l<=12):
+            factor = tmp[l]
+        else:
+            factor = (l-1) * (-1.4)
+        expected_energy = np.ones_like(np.asarray(iters)) * factor
 
+        expected_energy_list.append(expected_energy)
         iters_list.append(np.asarray(iters))
         energy_list.append(np.asarray(energy))
         sfs_fast_list.append(np.asarray(sfs_fast))
         xAxis_fast_list.append(np.asarray(xAxis_fast))
 
-    plot4me(iters_list, energy_list, text='Energy-Iteration')
-    plot4me(xAxis_fast_list, sfs_fast_list, text='operator-site')
+    plot4me(iters_list, energy_list, expected_energy_list, text='Energy-Iteration')
+    plot4me(xAxis_fast_list, sfs_fast_list, None, text='operator-site')
 
 
 
 #plot(dataname='run/L100.log', L=100)
 #plot(dataname='run/L20_estimate.log', L=20, observables=True)
 
-present(Ls=[10, 20, 30, 40], path='results/Gd')
+present(Ls=[6, 10, 15, 20], path='results/Sr')
