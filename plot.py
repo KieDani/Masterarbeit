a34e1140ca023b3f98e9e8261e00cfc5c1a6d84e
Daniel Kienzle <kienzledaniel@googlemail.com>
2021-03-10 15:39:56 +0100

    ...

--- plot.py
+++ plot.py
@@ -617,7 +617,9 @@ machine_names = ['JaxRBM', 'JaxFFNN', 'JaxDeepFFNN', 'JaxDeepConvNN', 'JaxSymmFF
 #plotObservables('run/secondResults/JaxDeepFFNN/L40_observables.csv', 40)
 #plotObservables('run/secondResults/JaxDeepConvNN/L60_observables.csv', 60)
 
-plotObservables('run/numberSamples_small/JaxDeepConvNN/L16_observables.csv', 16)
+
+#plotObservables('run/thirdResults/JaxDeepFFNN/L12_observables.csv', 12)
+#plotObservables('run/numberSamples_small/JaxDeepConvNN/L16_observables.csv', 16)
 
 #plot(dataname='run/secondResults/JaxDeepConvNN/L60.log', L=60)
 
