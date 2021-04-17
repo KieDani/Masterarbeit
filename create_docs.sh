427559da7bf9673271c8295c57d42e60ddf0fc66
Daniel Kienzle <kienzledaniel@googlemail.com>
2021-04-17 20:14:54 +0200

    ...

--- create_docs.sh
+++ create_docs.sh
@@ -2,5 +2,5 @@
 
 rm -r docs
 mkdir docs
-pdoc3 --html --output-dir docs test.py my_sampler.py my_operators.py my_models.py plot.py helping_functions.py my_machines.py 
+pdoc3 --html --output-dir docs test.py my_sampler.py my_operators.py my_models.py plot.py helping_functions.py my_machines.py
 
