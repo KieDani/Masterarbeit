81c868d1c162357cc22ddcd3ad1774954b38fc87
Daniel Kienzle <kienzledaniel@googlemail.com>
2021-06-01 15:46:52 +0200

    added dmrg-calculation of the ground state energy of the Haldane chain using dmrg4py

--- DMRG.py
+++ DMRG.py
@@ -1,2 +1,21 @@
+from dmrgpy import spinchain
+
+L=6
 
 
+spins = ["S=1" for i in range(L)] # S=1 chain
+sc = spinchain.Spin_Chain(spins) # create spin chain object
+h = 0 # initialize Hamiltonian
+for i in range(len(spins)-1):
+  h = h + sc.Sx[i]*sc.Sx[i+1]
+  h = h + sc.Sy[i]*sc.Sy[i+1]
+  h = h + sc.Sz[i]*sc.Sz[i+1]
+sc.set_hamiltonian(h)
+es = sc.get_excited(n=2,mode="DMRG")
+gap = es[1]-es[0] # compute gap
+print("Gap of the Haldane chain",gap)
+print('GS-Energy', es[0])
+#sc.get_dynamical_correlator(name=(sc.Sz[0],sc.Sz[0]))
+
+cs = [sc.vev(sc.Sz[0]*sc.Sz[i]).real for i in range(L)]
+print(cs)
