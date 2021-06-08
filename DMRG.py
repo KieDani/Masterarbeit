4795be07cdce31945b31bb4a04c68cdc745a100f
Daniel Kienzle <kienzledaniel@googlemail.com>
2021-06-08 14:34:42 +0200

    Now it is possible to calculate DMRG results for the Haldane chain using the skript DMRG.py

--- DMRG.py
+++ DMRG.py
@@ -1,57 +1,48 @@
 from dmrgpy import spinchain
 import dmrgpy.multioperator as mo
 
-L=40
+__L__=6
 
 
-#original Heisenberg
-spins = ["S=1" for i in range(L)] # S=1 chain
-sc = spinchain.Spin_Chain(spins) # create spin chain object
-h = 0 # initialize Hamiltonian
-for i in range(len(spins)-1):
-  h = h + sc.Sx[i]*sc.Sx[i+1]
-  h = h + sc.Sy[i]*sc.Sy[i+1]
-  h = h + sc.Sz[i]*sc.Sz[i+1]
-sc.set_hamiltonian(h)
+def get_original_Haldanechain(L = __L__):
+  #original Heisenberg
+  spins = ["S=1" for i in range(L)] # S=1 chain
+  sc = spinchain.Spin_Chain(spins) # create spin chain object
+  h = 0 # initialize Hamiltonian
+  for i in range(len(spins)-1):
+    h = h + sc.Sx[i]*sc.Sx[i+1]
+    h = h + sc.Sy[i]*sc.Sy[i+1]
+    h = h + sc.Sz[i]*sc.Sz[i+1]
+  sc.set_hamiltonian(h)
+  return sc
 
 
-#transformed Heisenberg
-spins = ["S=1" for i in range(L)] # S=1 chain
-sc2 = spinchain.Spin_Chain(spins) # create spin chain object
-h = 0 # initialize Hamiltonian
-for i in range(len(spins)-1):
-  expSx = -2* sc2.Sx[i+1]*sc2.Sx[i+1] + mo.obj2MO(1)
-  expSz = -2 * sc2.Sz[i] * sc2.Sz[i] + mo.obj2MO(1)
-  h = h + sc2.Sx[i]*sc2.Sx[i+1]
-  h = h + sc2.Sy[i]*expSz*expSx*sc2.Sy[i+1]
-  h = h + sc2.Sz[i]*sc2.Sz[i+1]
-sc2.set_hamiltonian(h)
+def get_transformed_Haldanechain(L=__L__):
+  #transformed Heisenberg
+  spins = ["S=1" for i in range(L)] # S=1 chain
+  sc2 = spinchain.Spin_Chain(spins) # create spin chain object
+  h = 0 # initialize Hamiltonian
+  for i in range(len(spins)-1):
+    expSx = -2* sc2.Sx[i+1]*sc2.Sx[i+1] + mo.obj2MO(1)
+    expSz = -2 * sc2.Sz[i] * sc2.Sz[i] + mo.obj2MO(1)
+    h = h + sc2.Sx[i]*sc2.Sx[i+1]
+    h = h + sc2.Sy[i]*expSz*expSx*sc2.Sy[i+1]
+    h = h + sc2.Sz[i]*sc2.Sz[i+1]
+  sc2.set_hamiltonian(h)
+  return sc2
 
 
-print(sc)
-print(sc.Sx)
-print(mo.write(sc.Sz[1], 'dummy.txt'))
+sc = get_transformed_Haldanechain()
+es = sc.get_excited(n=1,mode="DMRG")
+print(es)
+#gap = es[1]-es[0] # compute gap
+#print("Gap of the Haldane chain",gap)
+#print('GS-Energy', es[0])
+print('GS-Energy', es)
 
-print(sc2)
-print(sc2.Sx)
-print(mo.write(sc2.Sz[1], 'dummy.txt'))
-
-
-es = sc.get_excited(n=2,mode="DMRG")
-gap = es[1]-es[0] # compute gap
-print("Gap of the Haldane chain",gap)
-print('GS-Energy', es[0])
-
-
-es2 = sc2.get_excited(n=2,mode="DMRG")
-gap = es2[1]-es2[0] # compute gap
-print("Gap of the Haldane chain",gap)
-print('GS-Energy', es2[0])
 
 #sc.get_dynamical_correlator(name=(sc.Sz[0],sc.Sz[0]))
 
-cs = [sc.vev(sc.Sz[0]*sc.Sz[i]).real for i in range(L)]
+cs = [sc.vev(sc.Sz[0]*sc.Sz[i]).real for i in range(__L__)]
 print(cs)
 
-cs2 = [sc2.vev(sc2.Sz[0]*sc2.Sz[i]).real for i in range(L)]
-print(cs2)
