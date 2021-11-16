70994412ba3a2bb13826d8e720881682082946b1
kienzlda <daniel.kienzle@informatik.uni-augsburg.de>
2021-11-16 16:17:30 +0100

    added original and transformed AKLT to DMRG.py. Not tested yet

--- DMRG.py
+++ DMRG.py
@@ -10,12 +10,12 @@ def get_original_Haldanechain(L = __L__):
   #original Heisenberg
   spins = ["S=1" for i in range(L)] # S=1 chain
   sc = spinchain.Spin_Chain(spins) # create spin chain object
-  h = 0 # initialize Hamiltonian
+  H = 0 # initialize Hamiltonian
   for i in range(len(spins)-1):
-    h = h + sc.Sx[i]*sc.Sx[i+1]
-    h = h + sc.Sy[i]*sc.Sy[i+1]
-    h = h + sc.Sz[i]*sc.Sz[i+1]
-  sc.set_hamiltonian(h)
+    H += + sc.Sx[i]*sc.Sx[i+1]
+    H += + sc.Sy[i]*sc.Sy[i+1]
+    H += + sc.Sz[i]*sc.Sz[i+1]
+  sc.set_hamiltonian(H)
   return sc
 
 
@@ -23,14 +23,50 @@ def get_transformed_Haldanechain(L=__L__):
   #transformed Heisenberg
   spins = ["S=1" for i in range(L)] # S=1 chain
   sc2 = spinchain.Spin_Chain(spins) # create spin chain object
-  h = 0 # initialize Hamiltonian
+  H = 0 # initialize Hamiltonian
   for i in range(len(spins)-1):
+    h = 0
     expSx = -2 * sc2.Sx[i+1]*sc2.Sx[i+1] + mo.obj2MO(1)
     expSz = -2 * sc2.Sz[i] * sc2.Sz[i] + mo.obj2MO(1)
-    h = h + sc2.Sx[i]*sc2.Sx[i+1]
-    h = h + sc2.Sy[i]*expSz*expSx*sc2.Sy[i+1]
-    h = h + sc2.Sz[i]*sc2.Sz[i+1]
-  sc2.set_hamiltonian(h)
+    h += sc2.Sx[i]*sc2.Sx[i+1]
+    h += sc2.Sy[i]*expSz*expSx*sc2.Sy[i+1]
+    h += sc2.Sz[i]*sc2.Sz[i+1]
+    H += h
+  sc2.set_hamiltonian(H)
+  return sc2
+
+
+
+def get_original_AKLT(L=__L__):
+  #original AKLT
+  spins = ["S=1" for i in range(L)]  # S=1 chain
+  sc = spinchain.Spin_Chain(spins)  # create spin chain object
+  H = 0  # initialize Hamiltonian
+  for i in range(len(spins) - 1):
+    h = 0
+    h += + sc.Sx[i] * sc.Sx[i + 1]
+    h += + sc.Sy[i] * sc.Sy[i + 1]
+    h += + sc.Sz[i] * sc.Sz[i + 1]
+    H += h + 1./3 * h*h
+  sc.set_hamiltonian(H)
+  return sc
+
+
+
+def get_transformed_AKLT(L=__L__):
+  #transformed AKLT
+  spins = ["S=1" for i in range(L)] # S=1 chain
+  sc2 = spinchain.Spin_Chain(spins) # create spin chain object
+  H = 0 # initialize Hamiltonian
+  for i in range(len(spins)-1):
+    h = 0
+    expSx = -2 * sc2.Sx[i+1]*sc2.Sx[i+1] + mo.obj2MO(1)
+    expSz = -2 * sc2.Sz[i] * sc2.Sz[i] + mo.obj2MO(1)
+    h += sc2.Sx[i]*sc2.Sx[i+1]
+    h += sc2.Sy[i]*expSz*expSx*sc2.Sy[i+1]
+    h += sc2.Sz[i]*sc2.Sz[i+1]
+    H += h + 1./3 * h*h
+  sc2.set_hamiltonian(H)
   return sc2
 
 
